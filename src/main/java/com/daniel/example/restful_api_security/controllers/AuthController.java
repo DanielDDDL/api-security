2af993a3335098cb0aeff3b5092ff176396fafe4
DanielDDDL <daniel.dddl.pdf@gmail.com>
2018-04-25 19:13:05 -0300

    redirecting login end point

--- src/main/java/com/daniel/example/restful_api_security/controllers/AuthController.java
+++ src/main/java/com/daniel/example/restful_api_security/controllers/AuthController.java
@@ -3,12 +3,17 @@ package com.daniel.example.restful_api_security.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class AuthController {
 
     @GetMapping(value = {"/login", "/"})
-    public String loginPage() {
-        return "/login";
+    public String loginPage(Principal principal) {
+        if(principal == null)
+            return "/login";
+        else
+            return "redirect:/user/";
     }
 
 
